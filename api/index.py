3a27652f451f9058bf202c30c1802e40a6cdba34
Amogh Puthanikar <amoghpputhanikar1234@gmail.com>
2025-10-04 12:21:15 +0530

    Fixed Vercel deployment with Mangum

--- api/index.py
+++ api/index.py
@@ -1,10 +1,9 @@
 import sys
 import os
 
-# Add to path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app.main import app
+from mangum import Mangum
 
-# Export for Vercel ASGI
-app = app
+handler = Mangum(app, lifespan="off")
